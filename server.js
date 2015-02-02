a63a9ab2ae06405e5e16bed579dd2f0c2fe5aab1
Jeffrey Long <jlong@groupon.com>
2015-02-02 13:51:33 -0600

    Log req/res in server

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ var fs = require('fs');
 var http = require('http');
 
 var server = http.createServer(function(req, res){
+  console.log("<-- " + req.url);
   if(req.url === '/'){
     res.writeHead(200, {'Content-Type': 'text/html'});
+    console.log(" --> 200");
     fs.createReadStream('index.html').pipe(res);
   } else {
     res.writeHead(404, {'Content-Type': 'text/plain'});
+    console.log(" --> 404");
     res.end('file not found');
   }
 });
 
 server.listen(5678);
+console.log('Server up on port 5678…');
